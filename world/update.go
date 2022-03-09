715e73234850b8f23773b5d3fc6113adfcdbeb1a
nathan <86123602+noxJ@users.noreply.github.com>
2022-03-09 16:40:46 +0000

    Made them spawn automatically

--- world/update.go
+++ world/update.go
@@ -1,6 +1,18 @@
 package world
 
+var objTicker int = 0
+
 func Update() {
+	if objTicker <= 0 {
+		//r := rand.Intn(5) // more types later
+
+		NewObject(1240, 352, "column")
+
+		objTicker = 300
+	} else {
+		objTicker -= 1
+	}
+
 	for _, o := range Objects {
 		switch o.Type {
 		case "platform":
