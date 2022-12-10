8f8f1b31e33b872168990b03a61069561e45244d
Nathan <101135961+nath-ellis@users.noreply.github.com>
2022-12-10 14:14:19 +0000

    Added input prompts to the change colour option

--- player/colour.go
+++ player/colour.go
@@ -10,25 +10,27 @@ var (
 	limeIdle, _, _   = ebitenutil.NewImageFromFile("res/Lime/idle.png")
 	cyanIdle, _, _   = ebitenutil.NewImageFromFile("res/Cyan/idle.png")
 	yellowIdle, _, _ = ebitenutil.NewImageFromFile("res/Yellow/idle.png")
+	leftDark, _, _   = ebitenutil.NewImageFromFile("res/prompts/left_dark.png")
+	leftLight, _, _  = ebitenutil.NewImageFromFile("res/prompts/left_light.png")
+	rightDark, _, _  = ebitenutil.NewImageFromFile("res/prompts/right_dark.png")
+	rightLight, _, _ = ebitenutil.NewImageFromFile("res/prompts/right_light.png")
 )
+
+func ChangeColour() {
+	if Player.Colour == "lime" {
+		Player.Colour = "cyan"
+	} else if Player.Colour == "cyan" {
+		Player.Colour = "yellow"
+	} else if Player.Colour == "yellow" {
+		Player.Colour = "lime"
+	}
+}
 
 func ColourSelectorCtl() {
 	if inpututil.IsKeyJustPressed(ebiten.KeyRight) {
-		if Player.Colour == "lime" {
-			Player.Colour = "cyan"
-		} else if Player.Colour == "cyan" {
-			Player.Colour = "yellow"
-		} else if Player.Colour == "yellow" {
-			Player.Colour = "lime"
-		}
+		ChangeColour()
 	} else if inpututil.IsKeyJustPressed(ebiten.KeyLeft) {
-		if Player.Colour == "lime" {
-			Player.Colour = "yellow"
-		} else if Player.Colour == "cyan" {
-			Player.Colour = "lime"
-		} else if Player.Colour == "yellow" {
-			Player.Colour = "cyan"
-		}
+		ChangeColour()
 	}
 }
 
@@ -42,5 +44,24 @@ func DrawColourSelector(screen *ebiten.Image) {
 		screen.DrawImage(cyanIdle, op)
 	} else if Player.Colour == "yellow" {
 		screen.DrawImage(yellowIdle, op)
+	}
+
+	posX, posY := ebiten.CursorPosition()
+
+	op.GeoM.Scale(2, 2)
+	op.GeoM.Translate(0, 100)
+
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) || (posX > 5 && posX < 37) && (posY > 105 && posY < 137) {
+		screen.DrawImage(leftLight, op)
+	} else {
+		screen.DrawImage(leftDark, op)
+	}
+
+	op.GeoM.Translate(40, 0)
+
+	if ebiten.IsKeyPressed(ebiten.KeyRight) || (posX > 45 && posX < 77) && (posY > 105 && posY < 137) {
+		screen.DrawImage(rightLight, op)
+	} else {
+		screen.DrawImage(rightDark, op)
 	}
 }
