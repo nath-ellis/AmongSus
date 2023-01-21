9b6d0fa5fc407f0fc4a182791799a439075bbb52
Nathan <nathanellis15782@protonmail.com>
2023-01-21 14:07:25 +0000

    Changed the Object functions to be struct methods

--- world/objects.go
+++ world/objects.go
@@ -1,6 +1,7 @@
 package world
 
 import (
+	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/nath-ellis/AmongSus/space"
 	"github.com/solarlune/resolv"
 )
@@ -11,6 +12,67 @@ type Object struct {
 }
 
 var Objects []Object
+
+func (o *Object) Update() {
+	switch o.Type {
+	case "platform":
+		o.Obj.X -= Speed
+
+	case "column":
+		o.Obj.X -= Speed
+
+	case "spikes":
+		o.Obj.X -= Speed
+
+	case "turretbase":
+		o.Obj.X -= Speed
+	}
+
+	if o.Obj.X <= -o.Obj.W {
+		if o.Type == "platform" {
+			o.Obj.X = 1240
+		} else {
+			o.Remove()
+		}
+	}
+
+	o.Obj.Update()
+}
+
+func (o Object) Draw(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(o.Obj.X, o.Obj.Y)
+
+	switch o.Type {
+	case "platform":
+		screen.DrawImage(platform, op)
+
+	case "column":
+		screen.DrawImage(column, op)
+
+	case "spikes":
+		screen.DrawImage(spikes, op)
+
+	case "turretbase":
+		screen.DrawImage(turretbase, op)
+	}
+}
+
+func (o *Object) Remove() {
+	tmp := []Object{}
+
+	for _, O := range Objects {
+		if o.Obj.X == O.Obj.X && o.Type == O.Type {
+			continue
+		}
+		tmp = append(tmp, O)
+	}
+
+	space.Space.Remove(o.Obj)
+
+	Objects = []Object{}
+	Objects = tmp
+}
 
 func NewObject(x float64, y float64, Type string) {
 	newObjects := []Object{}
