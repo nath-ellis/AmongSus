f534ece19cf78f4d4b51fa7b16c673c705fadffc
Nathan <nathanellis15782@protonmail.com>
2023-01-28 15:01:26 +0000

    Added a button struct and converted buttons to it

--- player/colour.go
+++ player/colour.go
@@ -1,20 +1,7 @@
 package player
 
-import (
-	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-	"github.com/hajimehoshi/ebiten/v2/inpututil"
-)
-
-var (
-	leftDark, _, _   = ebitenutil.NewImageFromFile("res/prompts/left_dark.png")
-	leftLight, _, _  = ebitenutil.NewImageFromFile("res/prompts/left_light.png")
-	rightDark, _, _  = ebitenutil.NewImageFromFile("res/prompts/right_dark.png")
-	rightLight, _, _ = ebitenutil.NewImageFromFile("res/prompts/right_light.png")
-)
-
-func (p *PlayerData) ChangeColour(key ebiten.Key) {
-	if key == ebiten.KeyRight || key == ebiten.Key(ebiten.MouseButtonRight) {
+func (p *PlayerData) ChangeColour(direction string) {
+	if direction == "right" {
 		if p.Colour == "lime" {
 			p.Colour = "cyan"
 		} else if p.Colour == "cyan" {
@@ -24,7 +11,7 @@ func (p *PlayerData) ChangeColour(key ebiten.Key) {
 		}
 	}
 
-	if key == ebiten.KeyLeft || key == ebiten.Key(ebiten.MouseButtonLeft) {
+	if direction == "left" {
 		if p.Colour == "lime" {
 			p.Colour = "yellow"
 		} else if p.Colour == "cyan" {
@@ -35,38 +22,4 @@ func (p *PlayerData) ChangeColour(key ebiten.Key) {
 	}
 
 	p.LoadSprites()
-}
-
-func (p *PlayerData) ColourSelectorCtl() {
-	if inpututil.IsKeyJustPressed(ebiten.KeyRight) {
-		p.ChangeColour(ebiten.KeyRight)
-	} else if inpututil.IsKeyJustPressed(ebiten.KeyLeft) {
-		p.ChangeColour(ebiten.KeyLeft)
-	}
-}
-
-func (p *PlayerData) DrawColourSelector(screen *ebiten.Image) {
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(5, 5)
-
-	screen.DrawImage(p.IdleSprite, op)
-
-	posX, posY := ebiten.CursorPosition()
-
-	op.GeoM.Scale(2, 2)
-	op.GeoM.Translate(0, 100)
-
-	if ebiten.IsKeyPressed(ebiten.KeyLeft) || (posX > 5 && posX < 37) && (posY > 105 && posY < 137) {
-		screen.DrawImage(leftLight, op)
-	} else {
-		screen.DrawImage(leftDark, op)
-	}
-
-	op.GeoM.Translate(40, 0)
-
-	if ebiten.IsKeyPressed(ebiten.KeyRight) || (posX > 45 && posX < 77) && (posY > 105 && posY < 137) {
-		screen.DrawImage(rightLight, op)
-	} else {
-		screen.DrawImage(rightDark, op)
-	}
 }
--- world/menus.go
+++ world/menus.go
@@ -5,7 +5,9 @@ import (
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"github.com/hajimehoshi/ebiten/v2/text"
+	"github.com/solarlune/resolv"
 )
 
 var (
@@ -14,6 +16,41 @@ var (
 	click, _, _    = ebitenutil.NewImageFromFile("res/text/click-to-play.png")
 	menubg, _, _   = ebitenutil.NewImageFromFile("res/space.png")
 )
+
+type Button struct {
+	Obj           *resolv.Object
+	Sprite        *ebiten.Image
+	OnHoverSprite *ebiten.Image
+}
+
+func (b Button) IsPressed() bool {
+	mouseX, mouseY := ebiten.CursorPosition()
+
+	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) &&
+		float64(mouseX) > b.Obj.X && float64(mouseX) < b.Obj.X+b.Obj.W &&
+		float64(mouseY) > b.Obj.Y && float64(mouseY) < b.Obj.Y+b.Obj.H {
+		return true
+	}
+
+	return false
+}
+
+func (b Button) Draw(screen *ebiten.Image, scale bool) {
+	op := &ebiten.DrawImageOptions{}
+	if scale {
+		op.GeoM.Scale(2, 2)
+	}
+	op.GeoM.Translate(b.Obj.X, b.Obj.Y)
+
+	mouseX, mouseY := ebiten.CursorPosition()
+
+	if float64(mouseX) > b.Obj.X && float64(mouseX) < b.Obj.X+b.Obj.W &&
+		float64(mouseY) > b.Obj.Y && float64(mouseY) < b.Obj.Y+b.Obj.H {
+		screen.DrawImage(b.OnHoverSprite, op)
+	} else {
+		screen.DrawImage(b.Sprite, op)
+	}
+}
 
 func DrawMenu(screen *ebiten.Image, coins string) {
 	op := &ebiten.DrawImageOptions{}
