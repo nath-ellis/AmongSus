c482ca70758fb711f518e2274dcd44a3b03ce92e
Nathan <101135961+nath-ellis@users.noreply.github.com>
2023-01-28 15:01:26 +0000

    Added a button struct and converted buttons to it

--- main.go
+++ main.go
@@ -8,15 +8,21 @@ import (
 	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"github.com/nath-ellis/AmongSus/player"
 	"github.com/nath-ellis/AmongSus/space"
 	"github.com/nath-ellis/AmongSus/world"
+	"github.com/solarlune/resolv"
 )
 
 type Game struct{}
 
-var Player player.PlayerData
+var (
+	Player         player.PlayerData
+	LeftColourBtn  world.Button
+	RightColourBtn world.Button
+)
 
 func init() {
 	rand.Seed(time.Now().Unix())
@@ -25,66 +31,73 @@ func init() {
 	Player.Init()
 
 	world.Init()
+
+	leftDark, _, _ := ebitenutil.NewImageFromFile("res/prompts/left_dark.png")
+	leftLight, _, _ := ebitenutil.NewImageFromFile("res/prompts/left_light.png")
+	rightDark, _, _ := ebitenutil.NewImageFromFile("res/prompts/right_dark.png")
+	rightLight, _, _ := ebitenutil.NewImageFromFile("res/prompts/right_light.png")
+
+	LeftColourBtn = world.Button{
+		Obj:           resolv.NewObject(5, 100, 32, 32),
+		Sprite:        leftDark,
+		OnHoverSprite: leftLight,
+	}
+
+	RightColourBtn = world.Button{
+		Obj:           resolv.NewObject(55, 100, 32, 32),
+		Sprite:        rightDark,
+		OnHoverSprite: rightLight,
+	}
 }
 
 func (g *Game) Update() error {
 	if Player.State == "menu" {
-		if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
-			posX, posY := ebiten.CursorPosition()
-
-			if (posX > 5 && posX < 37) && (posY > 105 && posY < 137) { // Left Button
-				Player.ChangeColour(ebiten.Key(ebiten.MouseButtonLeft))
-			} else if (posX > 45 && posX < 77) && (posY > 105 && posY < 137) { // Right Button
-				Player.ChangeColour(ebiten.Key(ebiten.MouseButtonRight))
-			} else {
-				Player.State = "game"
-			}
+		if LeftColourBtn.IsPressed() {
+			Player.ChangeColour("left")
+		} else if RightColourBtn.IsPressed() {
+			Player.ChangeColour("right")
+		} else if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
+			Player.State = "game"
 		}
-		Player.ColourSelectorCtl()
 	} else if Player.State == "game" {
 		Player.Controls()
 		world.Update()
 	} else if Player.State == "gameOver" {
-		if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
-			posX, posY := ebiten.CursorPosition()
+		if LeftColourBtn.IsPressed() {
+			Player.ChangeColour("left")
+		} else if RightColourBtn.IsPressed() {
+			Player.ChangeColour("right")
+		} else if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
+			// Reset the game
+			Player.Obj.X = 50
+			Player.Obj.Y = -100
 
-			if (posX > 5 && posX < 37) && (posY > 105 && posY < 137) { // Left Button
-				Player.ChangeColour(ebiten.Key(ebiten.MouseButtonLeft))
-			} else if (posX > 45 && posX < 77) && (posY > 105 && posY < 137) { // Right Button
-				Player.ChangeColour(ebiten.Key(ebiten.MouseButtonRight))
-			} else {
-				// Reset the game
-				Player.Obj.X = 50
-				Player.Obj.Y = -100
+			world.Speed = 2
+			world.ObjTicker = 0
+			world.MaxObjTicker = 250
 
-				world.Speed = 2
-				world.ObjTicker = 0
-				world.MaxObjTicker = 250
-
-				// Remove all objects
-				for _, o := range world.Objects {
-					space.Space.Remove(o.Obj)
-				}
-
-				world.Objects = []world.Object{}
-				world.NewObject(0, 476, "platform")
-				world.NewObject(124, 476, "platform")
-				world.NewObject(248, 476, "platform")
-				world.NewObject(372, 476, "platform")
-				world.NewObject(496, 476, "platform")
-				world.NewObject(620, 476, "platform")
-				world.NewObject(744, 476, "platform")
-				world.NewObject(868, 476, "platform")
-				world.NewObject(992, 476, "platform")
-				world.NewObject(1116, 476, "platform")
-				world.NewObject(1240, 476, "platform")
-
-				world.Coins = []world.Coin{}
-
-				Player.State = "game"
+			// Remove all objects
+			for _, o := range world.Objects {
+				space.Space.Remove(o.Obj)
 			}
+
+			world.Objects = []world.Object{}
+			world.NewObject(0, 476, "platform")
+			world.NewObject(124, 476, "platform")
+			world.NewObject(248, 476, "platform")
+			world.NewObject(372, 476, "platform")
+			world.NewObject(496, 476, "platform")
+			world.NewObject(620, 476, "platform")
+			world.NewObject(744, 476, "platform")
+			world.NewObject(868, 476, "platform")
+			world.NewObject(992, 476, "platform")
+			world.NewObject(1116, 476, "platform")
+			world.NewObject(1240, 476, "platform")
+
+			world.Coins = []world.Coin{}
+
+			Player.State = "game"
 		}
-		Player.ColourSelectorCtl()
 	}
 
 	return nil
@@ -93,13 +106,27 @@ func (g *Game) Update() error {
 func (g *Game) Draw(screen *ebiten.Image) {
 	if Player.State == "menu" {
 		world.DrawMenu(screen, fmt.Sprint(Player.Coins))
-		Player.DrawColourSelector(screen)
+
+		op := &ebiten.DrawImageOptions{}
+		op.GeoM.Translate(5, 5)
+
+		screen.DrawImage(Player.IdleSprite, op)
+
+		LeftColourBtn.Draw(screen, true)
+		RightColourBtn.Draw(screen, true)
 	} else if Player.State == "game" {
 		world.Draw(screen)
 		Player.Draw(screen)
 	} else if Player.State == "gameOver" {
 		world.DrawGameOver(screen, fmt.Sprint(Player.Coins))
-		Player.DrawColourSelector(screen)
+
+		op := &ebiten.DrawImageOptions{}
+		op.GeoM.Translate(5, 5)
+
+		screen.DrawImage(Player.IdleSprite, op)
+
+		LeftColourBtn.Draw(screen, true)
+		RightColourBtn.Draw(screen, true)
 	}
 }
 
